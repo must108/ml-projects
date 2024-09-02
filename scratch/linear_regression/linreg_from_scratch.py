5923471c03f9973cbb9eba0e91b811d2bb135eb9
must108 <mustaeen18@gmail.com>
2024-09-01 22:33:14 -0400

    finished lin reg implementation

--- scratch/linear_regression/linreg_from_scratch.py
+++ scratch/linear_regression/linreg_from_scratch.py
@@ -1,52 +1,40 @@
 import pandas as pd
 import numpy as np
+from sklearn.linear_model import LinearRegression
 
 teams = pd.read_csv("./teams.csv")
 
+# set X and Y matricies to respective columns
 X = teams[["athletes", "prev_medals"]].copy()
-Y = teams[["medals"]].copy()
+y = teams[["medals"]].copy()
 
-# matrix multiplication:
+X["intercept"] = 1
+X = X[["intercept", "athletes", "prev_medals"]]
 
-# rows of first matrix and cols of second matrix is dimensions of the
-# resulting matrix
+X_T = X.transpose()  # transpose of the matrix
 
-# to transpose a matrix is to essentially: make all rows into columns
-# mutliplying a matrix by its transpose leaves you with a square matrix
-# you can invert a square matrix!
+# formula for calculating coefficients for linear regression
+B = np.linalg.inv(X_T @ X) @ X_T @ y
+B.index = X.columns  # so the dataframe is properly labelled
+# inverse of the XTX matrix (@ is a matrix multiplier)
 
-# an inverted matrix has the same size as the square matrix
-# if you multiply a matrix by its inverse, you end up with the
-# "identity matrix"
+predictions = X @ B  # represents y = XB, X matrix times coefficient matrix
 
-# if you multiply an identity matrix by its original matrix,
-# you get the same original matrix as a result
-# like multiplying by 1
+# calculate sum of squared residuals
+SSR = ((y - predictions) ** 2).sum()
+SST = ((y - y.mean()) ** 2).sum()
 
-# to solve for linear regression, put vals in a matrix (b0, b1, e)
-# do this to solve for our coefficients (b0, b1, or B)
-# each value can be represented by a matrix
-# matrix of y vals = (matrix of x) * (matrix of B) + (matrix of e)
+R2 = 1 - (SSR/SST)
+# was at 0.87, so its a pretty good fit!
 
-# this can be simplified to y = XB + e
-# e can be dropped, as we're trying to minimize error
+# test with sklearn
 
-# y = XB
-# we want to cancel out X by multiplying by its inverse
-# but since X isnt a square matrix, we multiply by its transpose
+lr = LinearRegression()
 
-# (XT = transpose)
-# XTy = XTX B
+X2 = teams[["athletes", "prev_medals"]]
+y2 = teams[["medals"]]
 
-# now, XTX = square matrix.
-
-# (XTX)-1 XTy = (XTX)-1 XTX B
-# multiply by the inverse of XTX, as XTX is square
-
-# identity matrix is 1, so XTX is cancelled out
-
-# (XTX)-1 XTy = IB (where I is the identity matrix)
-# (XTX)-1 XTy = B (as the identity matrix can be treated as 1)
-
-# B = (XTX)-1 XTy
-# our final formula, solves for lin reg coefficients
+lr.fit(X2, y2)
+print(lr.intercept_)
+print(lr.coef_)
+# same as our algorithm
